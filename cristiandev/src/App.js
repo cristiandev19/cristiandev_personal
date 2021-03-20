a29d431ee02360223da0951539a05f1a8d255724
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-19 22:23:49 -0500

    importando estilos y maquetando

--- cristiandev/src/App.js
+++ cristiandev/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import './styles/utils.css'
+import './styles/theme.css'
 
 function App() {
   return (
     <div className="App">
-      <div className="Row">
-        <figure>
-          <img src={process.env.PUBLIC_URL + '/logo192.png'} alt=""/>
+      <div className="f-center">
+        <figure className="w-70 m-auto">
+          <img src={process.env.PUBLIC_URL + '/logo900.png'} alt="" className="w-100"/>
         </figure>
       </div>
-      <div className="Row f-column">
-        <h2>¡Hola a todos!</h2>
-        <h1>Soy Cristian Sotomayor</h1>
-        <p>Un fullstack developer autodidacta por pasión, vocación y profesión. Me encanta ser retado constantemente a superar mis miedos y limites.</p>
-        <button>Descargar CV</button>
+      <div className="f-content-center">
+        <div className="w-80 m-auto">
+          <h2>¡Hola a todos!</h2>
+          <h1>Soy Cristian Sotomayor</h1>
+          <p>
+            Un fullstack developer autodidacta por pasión, vocación y profesión. Me encanta ser retado constantemente a superar mis miedos y limites.
+          </p>
+          <button className="primary-btn">Descargar CV</button>
+        </div>
       </div>
     </div>
   );
